f41b8d8e638feedd7455db7b88c3a8a92478ade2
Ivan Esin <ivan.esin.78@mail.ru>
2023-07-19 18:06:07 +0400

    first commit

---
+++ Семинары/Homework/JS/main.js
@@ -0,0 +1,142 @@
+
+// let headers = document.getElementById('title__br')
+
+// headers.style.color = "blue"
+
+// function MyFunc(){
+//     const testvariable = 'dfyz tcby rehfktcby'
+//     document.write(testvariable)
+// }
+// const newvariables = [1,2,3,4,5,65,117]
+// newvariables.sort()
+// newvariables.reverse()
+
+// console.log(newvariables)
+// document.write(newvariables)
+// alert(newvariables)
+
+// задание 1
+ 
+// let a = 158
+// let b = 2
+// alert(a+b)
+
+
+
+
+// задание 2
+
+// alert("Првиет мир!")
+
+
+
+// задание 3
+
+// let n = prompt("Введите имя")
+// alert(n)
+
+
+// задание 4
+
+// function Name(){
+//     let name = prompt("Введите имя")
+//     alert("Првиет," +" "+ name)
+// }
+
+
+
+// задание 5
+// function showDialog() {
+//     let result = confirm("Вы уверены?");
+    
+//     if (result === true) {
+//       alert("Мы рады, что Вы уверены!");
+//     } else {
+//       alert("Жаль, что Вы не уверены…");
+//     }
+// }
+
+ 
+// задание 6
+
+
+// let age = parseInt(prompt("Введите возраст"));
+
+// switch(age){
+
+//     case 18:
+//         alert('Вы совершеннолетний, все можно!');
+//         break;
+
+//     case 10:
+//         alert('Вам надо учить уроки!');
+//         break;
+
+//     case 30:
+//         alert('Ложитесь спать, завтра на работу');
+//         break; 
+
+//     default :
+//         alert('Мы не знаем что Вам делать');
+// }
+
+// // задание 7
+
+// let array1 = [1,2,3,4,5,65,117,-1,3]
+// array1.sort(function(a,b){
+//     return a - b;
+// });
+// console.log(array1)
+
+
+// // задание 8
+
+// let array2 = ['a', 'b', 'c'];
+
+// array[0] = 'winner';
+// array[1] = 'i go alone';
+// array[2] = 'L';
+
+// console.log(array2)
+
+// // задание 9
+
+// let array3 = [1,1124,43,7,32,7,9]
+
+// for (let index = 0; index < array3.length; index++) {
+//     array3[index]++;
+// }
+// console.log(array3)
+
+
+
+// ---------------------Домашнее задание---------------------
+
+// Task1
+
+function Mymovie() {
+
+    let name = prompt("Введите название моего любимого фильма")
+    let movie = "Пролетая над гнездом кукушки"
+
+    if (name == movie) {
+      alert("Верно!");
+    } else {
+      alert("Попробуйте ещё раз");
+    }
+}
+
+// Task2
+
+let array = ['a', 'b', 'c'];
+
+array[0] = 'winner';
+array[1] = 'i go alone';
+array[2] = 'L';
+
+console.log(array);
+alert(array);
+
+
+ 
+
